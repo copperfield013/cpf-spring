45dc4625933b4732e6fc50ea048960a66f4e11c8
copperfield <copperfield@db9ca999-bb5a-44c0-85d8-baf245d6434d>
2018-04-18 09:15:15 +0000

    git-svn-id: http://47.100.187.235:13932/svn/cpf-spring@5 db9ca999-bb5a-44c0-85d8-baf245d6434d

---
+++ src/main/java/cn/sowell/copframe/spring/propTranslator/translator/AbstractPropertyValueTranslator.java
@@ -0,0 +1,15 @@
+package cn.sowell.copframe.spring.propTranslator.translator;
+
+import cn.sowell.copframe.spring.propTranslator.PropertyValueTranslator;
+
+/**
+ * 
+ * <p>Title: AbstractPropertyValueTranslator</p>
+ * <p>Description: </p><p>
+ * 
+ * </p>
+ * @author Copperfield Zhang
+ * @date 2017年12月28日 上午9:12:05
+ */
+public abstract class AbstractPropertyValueTranslator implements PropertyValueTranslator{
+}
